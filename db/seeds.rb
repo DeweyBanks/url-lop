6e48a3c56d599a30f99f956a39746d92383d5fbe
deweybanks <deweybanks@gmail.com>
2018-03-25 17:31:10 -0400

    adds seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+links = Link.create([
+  {given_url: "google.com", clicks: rand(100)},
+  {given_url: "yahoo.com", clicks: rand(100)},
+  {given_url: "imdb.com", clicks: rand(100)},
+  {given_url: "dewey.com", clicks: rand(100)},
+  {given_url: "facbook.com", clicks: rand(100)},
+  {given_url: "linkedin.com", clicks: rand(100)},
+  {given_url: "github.com", clicks: rand(100)},
+  {given_url: "netflix.com", clicks: rand(100)},
+  {given_url: "http://www.fxnetworks.com/shows/baskets", clicks: rand(100)},
+  {given_url: "http://www.amc.com/shows/the-walking-dead", clicks: rand(100)},
+  {given_url: "http://vfemmes.com", clicks: rand(100)},
+  {given_url: "http://ween.com", clicks: rand(100)},
+  {given_url: "https://survey-constructor.herokuapp.com", clicks: rand(100)},
+  {given_url: "https://illcreative.com", clicks: rand(100)},
+  {given_url: "https://www.olly.com", clicks: rand(100)},
+  {given_url: "https://www.alktheatercompany.com", clicks: rand(100)},
+  {given_url: "youtube.com", clicks: rand(100)},
+  {given_url: "https://www.thetoptens.com/", clicks: rand(100)},
+  {given_url: "https://www.wikipedia.org", clicks: rand(100)},
+  {given_url: "amazon.com", clicks: rand(100)},
+  {given_url: "twitter.com", clicks: rand(100)},
+  {given_url: "reddit.com", clicks: rand(100)},
+  {given_url: "instagram.com", clicks: rand(100)},
+  {given_url: "ebay.com", clicks: rand(100)},
+  ])

f3ad901e9889bfb4c4b11e67953039707b94b9bb
deweybanks <deweybanks@gmail.com>
2018-03-25 13:28:47 -0400

    removes default from database yml

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -91,6 +91,6 @@ class LinksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def link_params
-      params.require(:link).permit(:given_url, :slug, :display_url, :clicks, :title)
+      params.require(:link).permit(:given_url, :slug, :clicks, :title)
     end
 end
